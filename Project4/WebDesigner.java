1b6c2c52595d548dd4406630d7d5a86006e3f8ee
Scott Nidell <snidell_2000@yhoo.com>
2014-04-01 22:09:20 -0500

    Added Notes to WebDesigner

--- Project4/WebDesigner.java
+++ Project4/WebDesigner.java
@@ -1,6 +1,7 @@
 /*************************************************************************
  *  Compilation:  javac WebDesigner.java <br>
  *  Execution:    java WebDesigner <br>
+ *  Created By: Scott Nidell March 15th 2014
  *************************************************************************/
 
 import java.text.*;
